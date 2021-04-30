810b2e6e7b890b7f2ecf10e661a4e480e1be8646
JeromeC <nathan_9_9@hotmail.com>
2021-04-30 15:26:12 -0400

    DOM intro

---
+++ Scripts/app.js
@@ -0,0 +1,31 @@
+//IIFE
+(function(){
+
+
+   // Named Fuction
+    function Start()
+    {
+     
+       console.log("App Started"); 
+       
+    }
+
+    
+
+    //declareative format
+    
+    
+    // let Start = function(){
+    //     console.log("App Started");
+    // }
+
+    //Arrow Fuction NEWEST
+    /* let Start = ()=>
+    {
+        console.log("App Stared");
+    } */
+
+
+
+    window.addEventListener("load", Start);
+})();
