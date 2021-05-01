1a5cfd7edca2ee6278d70475a9be787379d019a8
JeromeC <nathan_9_9@hotmail.com>
2021-05-01 14:58:39 -0400

    .nojekyll added

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,3 +1,4 @@
+
 //IIFE
 (function(){
 
@@ -5,27 +6,67 @@
    // Named Fuction
     function Start()
     {
-     
-       console.log("App Started"); 
+       console.log('%cApp Started', "color:red; font-size: 24px;"); 
+       let clickMeButton = document.getElementById("clickMeButton");
+       clickMeButton.addEventListener("click", clickBtn, false);
+
+    //    if(clickMeButton)
+    //    {
+    //        console.log(clickMeButton);
+    //    }
+       let buttons = document.getElementsByTagName("button");
+
+       if (buttons)
+       {
+           for (const button of buttons) {
+               console.log(button);
+           }
+           //console.log(buttons);
+       }
+
+       let buttonsQuery = document.querySelectorAll("button");
        
+       if (buttonsQuery)
+       {
+           for (let index = 0; index < buttonsQuery.length; index++) {
+               const element = buttonsQuery[index];
+               console.log(buttonsQuery[index]);
+               
+           }
+       }
+
+       let helloWorld = document.getElementsByTagName("h1")[0].textContent;
+       console.log(helloWorld);
+       let mainInnerHTML = document.getElementsByTagName("main")[0].innerHTML;
+       console.log(mainInnerHTML);
+       
+
+    //    if(buttonsQuery)
+    //    {
+    //        buttonsQuery.forEach(butt => {
+    //            console.log(butt);
+    //        });
+    //    }
+
+    //    let clickMeButtons = document.getElementsByName("myBtn");
+    //    if(clickMeButtons)
+    //    {
+    //        console.log(clickMeButtons);
+    //    }
+
+    //    let documentlocation = document.location;
+    //    let documentURL = document.URL;
+    //    let documentreferrer = document.referrer;
+    //    console.log(documentlocation);
+    //    console.log(documentURL);
+    //    console.log(documentreferrer);
     }
-
     
-
-    //declareative format
-    
-    
-    // let Start = function(){
-    //     console.log("App Started");
-    // }
-
-    //Arrow Fuction NEWEST
-    /* let Start = ()=>
-    {
-        console.log("App Stared");
-    } */
-
 
 
     window.addEventListener("load", Start);
 })();
+
+function clickBtn(){
+    console.log("Stop Clicking Me");
+}
